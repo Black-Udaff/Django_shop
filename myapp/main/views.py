5d89a8f67b593f14b608c494066b11ff49ba1f97
Black-Udaff <xxxalex_93@icloud.com>
2024-06-17 00:04:43 +0300

    first

---
+++ myapp/main/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+
+    context = {
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
+
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'ПРОДАЕМ ГАВНО'
+    }
+    return render(request, 'main/about.html', context)
