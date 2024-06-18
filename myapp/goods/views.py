6c56ad612adb95e8fe5d06159b39f1c870cca452
Black-Udaff <xxxalex_93@icloud.com>
2024-06-19 00:08:05 +0300

    goods end

--- myapp/goods/views.py
+++ myapp/goods/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-
+from .goods_list import goods
 # Create your views here.
+
+
 def catalog(request):
-    return render()
+    context = {
+        'title': 'Home - Каталог',
+        'goods': goods,
+
+
+    }
+    return render(request, 'goods/catalog.html', context)
 
 
 def product(request):
-    return render()
+    return render(request, 'goods/product.html')
