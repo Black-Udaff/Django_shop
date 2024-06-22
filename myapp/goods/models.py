8951bd145d4a585b411ad77b48b65a4ed5af001a
Black-Udaff <xxxalex_93@icloud.com>
2024-06-23 02:37:34 +0300

    debag-tool-bar add

--- myapp/goods/models.py
+++ myapp/goods/models.py
@@ -10,13 +10,16 @@ class Categories(models.Model):
         db_table = 'category'
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+    
+    def __str__(self) -> str:
+        return self.name
 
 
 class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
-    image = models.ImageField(upload_to='goods_images', null=True, verbose_name='Картинка')
+    image = models.ImageField(upload_to='goods_images', null=True, blank=True, verbose_name='Картинка')
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
     discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в %')
     quantity = models.PositiveBigIntegerField(default=0, verbose_name='Колличество')
@@ -26,3 +29,6 @@ class Products(models.Model):
         db_table = 'product'
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+    def __str__(self) -> str:
+        return f'{self.name} Колличество - {self.quantity}'
--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 
     'main',
     'goods',
+
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'myapp.urls'
@@ -129,3 +133,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
